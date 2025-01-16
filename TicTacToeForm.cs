0f0c69cf87c458fb3a282a2fd06478243c8d17d0
YoyoXD <146937662+YoyoNinovD@users.noreply.github.com>
2025-01-16 22:12:33 +0200

    Add project files.

---
+++ TicTacToeForm.cs
@@ -0,0 +1,10 @@
+namespace TicTacToe___Individual_Project
+{
+    public partial class TicTacToeForm : Form
+    {
+        public TicTacToeForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
